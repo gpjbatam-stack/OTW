3979961aaa7b41c1fe1e2560438ca273d5bb8fea
gpjbatam-stack <gpj.batam@gmail.com>
2026-08-18 00:44:59 +0700

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://vumyxlbyhlaicubtgun.supabase.co";
+const SUPABASE_URL = "https://vumyxlbybhlaicubtgun.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_I_RUzlQZGzVChVl7gbku6Q_Nmq1FBD2";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
